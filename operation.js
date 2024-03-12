c95a179278bf7d63e4017e7748ebd6cc036a226d
devlrakib <devrakib5@gmail.com>
2024-03-12 19:27:16 +0600

    first commit

---
+++ operation.js
@@ -0,0 +1,23 @@
+var onionPrice = 20;
+var eggPrice   = 10;
+/*console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice+eggPrice);*/
+
+// Addition and subtraction Javascript Code 
+var totalPrice = onionPrice+eggPrice;
+console.log(totalPrice);
+var priceDifference = onionPrice-eggPrice;
+console.log(priceDifference);
+
+// Multiplies Javascript 
+var orangePrice = 20;
+var quantity    = 7;
+var totalCost   = orangePrice*quantity;
+console.log(totalCost);
+
+// Division Javascript
+var money = 500;
+var player = 10;
+var eachPlayer = money/player;
+console.log(eachPlayer);
